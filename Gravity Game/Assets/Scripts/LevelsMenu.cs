0edf2e380b432c86210aaa1faf94c88ed65c9185
Connor Heath <cmheath2303@yahoo.com>
2019-10-30 11:36:47 -0700

    Levels and skins menu

---
+++ Gravity Game/Assets/Scripts/LevelsMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelsMenu : MonoBehaviour
+{
+    public GameObject levelsButtonPrefab;
+    public GameObject levelsButtonContainer;
+
+    private void Start()
+    {
+        Sprite[] thumbnails = Resources.LoadAll<Sprite>("Levels");
+        foreach (Sprite thumbnail in thumbnails)
+        {
+            GameObject container = Instantiate(levelsButtonPrefab) as GameObject;
+            container.GetComponent<Image>().sprite = thumbnail;
+        }
+    }
+
+
+}
---
+++ Gravity Game/Assets/Scripts/SkinsMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinsMenu : MonoBehaviour
+{
+    public GameObject skinsButtonPrefab;
+    public GameObject skinsButtonContainer;
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+        /*private Sprite[] textures = Resources.LoadAll<Sprite>("Skins");
+        foreach (Sprite texture in textures)
+        {
+            GameObject container = Instantiate(skinsButtonPrefab) as GameObject;
+            container.GetComponent<Image>().sprite = texture;
+            container.transform.SetParent(skinsButtonContainer.transform,false);
+
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
