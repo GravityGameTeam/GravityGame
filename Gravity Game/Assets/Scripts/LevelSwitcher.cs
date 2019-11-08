a2197a25fc89df40d6d9539f7b8fce44d30d00ea
Finn Bromenschenkel <fentonpb2@gmail.com>
2019-11-08 12:04:26 -0800

    Edited level issues and level prefabs

--- Gravity Game/Assets/Scripts/LevelSwitcher.cs
+++ Gravity Game/Assets/Scripts/LevelSwitcher.cs
@@ -1,29 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSwitcher : MonoBehaviour
 {
-    public List<GameObject> levels = new List<GameObject>();
-    
+    public List<GameObject> levelPrefabs = new List<GameObject>();
+
+    private List<GameObject> levels = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        SelectLevel(1);
+        foreach (GameObject levelPrefab in levelPrefabs)
+        {
+            GameObject newLevel = Instantiate(levelPrefab);
+            newLevel.SetActive(false);
+            levels.Add(newLevel);
+        }
     }
 
     // Update is called once per frame
     public void ClearLevels()
     {
+        /*
         foreach (GameObject level in levels)
         {
             level.SetActive(false);
         }
+        */
     }
 
-    public void SelectLevel(int level)
+    public void SelectLevel(int levelNumber)
     {
-        ClearLevels();
-        levels[level - 1].SetActive(true);
+        levels[levelNumber - 1].SetActive(true);
+        SceneManager.MoveGameObjectToScene(levels[levelNumber - 1], SceneManager.GetSceneByName("Game"));
+        Debug.Log("Moved level object?");
+        
+        //ClearLevels();
+        //levels[level - 1].SetActive(true);
+    }
+
+    public void InstantiateLevels()
+    {
+        for (int i = 0; i < levelPrefabs.Count; i ++)
+        {
+            levels[i] = Instantiate(levelPrefabs[i]);
+        }
     }
 }
