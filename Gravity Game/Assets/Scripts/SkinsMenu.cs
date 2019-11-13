0b119776754237c0ab5bc3c24ec8248b1139c1d1
Connor Heath <cmheath2303@yahoo.com>
2019-11-13 10:31:06 -0800

    Added Pictures

--- Gravity Game/Assets/Scripts/SkinsMenu.cs
+++ Gravity Game/Assets/Scripts/SkinsMenu.cs
@@ -1,29 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SkinsMenu : MonoBehaviour
 {
     public GameObject skinsButtonPrefab;
     public GameObject skinsButtonContainer;
-    // Start is called before the first frame update
+
     private void Start()
     {
-
-        /*private Sprite[] textures = Resources.LoadAll<Sprite>("Skins");
-        foreach (Sprite texture in textures)
+        Sprite[] thumbnails = Resources.LoadAll<Sprite>("Skins");
+        foreach (Sprite thumbnail in thumbnails)
         {
             GameObject container = Instantiate(skinsButtonPrefab) as GameObject;
-            container.GetComponent<Image>().sprite = texture;
-            container.transform.SetParent(skinsButtonContainer.transform,false);
+            container.GetComponent<Image>().sprite = thumbnail;
+            container.transform.SetParent(skinsButtonContainer.transform, false);
 
-        }*/
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
