d0f9d0e53fe0e7d7e3cc8be12dadd7c72c1dcf7a
Finn Bromenschenkel <fentonpb2@gmail.com>
2020-01-07 10:44:36 -0800

    Added level 6

--- Gravity Game/Assets/Scripts/PlayerData.cs
+++ Gravity Game/Assets/Scripts/PlayerData.cs
@@ -7,14 +7,14 @@ public class PlayerData
 {
     static public int selectedLevel = 1;
     static public int farthestLevel = 1;
-    static public readonly int numberOfLevels = 5;
+    static public readonly int numberOfLevels = 6;
     
     static public string skinPick = "";
     
     static public Vector2 spawnPoint = new Vector2(0f, 0f);
 
-    static public List<float> scores = new List<float> {0f, 0f, 0f, 0f, 0f};
-    static public List<float> highScores = new List<float> {1000000f, 1000000f, 1000000f, 1000000f, 1000000f};
+    static public List<float> scores = new List<float> {0f, 0f, 0f, 0f, 0f, 0f};
+    static public List<float> highScores = new List<float> {1000000f, 1000000f, 1000000f, 1000000f, 1000000f, 1000000f};
 
-    static private List<float> allTimeRecords = new List<float> {0.81f, 1.11f, 3.02f, 6.39f, 45.53f};
+    static private List<float> allTimeRecords = new List<float> {0.81f, 1.11f, 3.02f, 6.39f, 45.53f, 1000000f};
 }
