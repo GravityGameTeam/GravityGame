406b1f7010533221e38fe8e9aa108490c0c9c961
Finn Bromenschenkel <fentonpb2@gmail.com>
2020-02-06 10:54:08 -0800

    Added music switching

---
+++ Gravity Game/Assets/Scripts/MusicChanger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MusicChanger : MonoBehaviour
+{
+    private AudioSource[] audioSources;
+    private int track = 0;
+
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        audioSources = GetComponents<AudioSource>(); //accesses all AudioSources
+        SetTrack(track);
+        SetVol(0.5f);
+    }
+    
+    public void SetVol(float vol)
+    {
+        foreach (AudioSource source in audioSources)
+        {
+            source.volume = vol;
+        }
+    }
+
+    private void Mute()
+    {
+        Debug.Log("Deactivating music");
+        foreach (AudioSource source in audioSources)
+        {
+            source.enabled = false;
+            Debug.Log("Deactivated track");
+        }
+    }
+
+    public void SetTrack(int track)
+    {
+        Mute();
+
+        audioSources[track].enabled = true;
+    }
+}
---
+++ Gravity Game/Assets/Scripts/MusicMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MusicMenu : MonoBehaviour
+{
+    public GameObject MusicButtonPrefab;
+    public GameObject MusicButtonContainer;
+    public GameObject MusicPlayer;
+
+    private void Start()
+    {
+        //procedurally generates each music button in a row
+        Sprite[] thumbnails = Resources.LoadAll<Sprite>("Music");
+        foreach (Sprite thumbnail in thumbnails)
+        {
+            GameObject container = Instantiate(MusicButtonPrefab) as GameObject;
+            container.GetComponent<Image>().sprite = thumbnail;
+            container.transform.SetParent(MusicButtonContainer.transform, false);
+
+            string track = thumbnail.name;
+            container.GetComponent<Button>().onClick.AddListener(() => MusicLoad(track));
+        }
+    }
+
+    //sets the target level using PlayerData, then switches scenes. Game scene loads level on Start.
+    private void MusicLoad(string track)
+    {
+        //assigns selectedLevel based on name of image file in button clicked
+        int trackToLoad;
+        int.TryParse(track, out trackToLoad);
+        MusicPlayer.GetComponent<MusicChanger>().SetTrack(trackToLoad);
+
+        //changes music
+        //SceneManager.LoadScene("Game");
+        Debug.Log("Loaded music");
+        Debug.Log(track);
+    }
+}
