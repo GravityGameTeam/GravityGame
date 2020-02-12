34f1aa96897657900ff7f87b138502e23704d6c3
Finn Bromenschenkel <fentonpb2@gmail.com>
2020-02-12 11:14:57 -0800

    Figured out how to limit scrolling. It's insane, but it works.

---
+++ Gravity Game/Assets/Scripts/ScrollControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollControl : MonoBehaviour
+{
+    public float leftLimit;
+    public float rightLimit;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(rightLimit, transform.position.y, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(transform.position);
+
+        if (transform.position.x < leftLimit)
+        {
+            transform.position = new Vector3(leftLimit, transform.position.y, 0);
+        } else if (transform.position.x > rightLimit)
+        {
+            transform.position = new Vector3(rightLimit, transform.position.y, 0);
+        }
+    }
+}
