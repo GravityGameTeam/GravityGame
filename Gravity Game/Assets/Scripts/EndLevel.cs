9d8a425dd3275f8cd6d18495a219641872af3c18
Cael <itscael@gmail.com>
2020-02-21 10:23:34 -0800

    Quite a bit

--- Gravity Game/Assets/Scripts/EndLevel.cs
+++ Gravity Game/Assets/Scripts/EndLevel.cs
@@ -11,10 +11,15 @@ public class EndLevel : MonoBehaviour
     public GameObject timerManager;
     public GameObject player;
     public GameObject scoringManager;
+    public GameObject pausePanel;
+    
+    //Animations
+    public Animator compAnim;
 
     // Start is called before the first frame update
     public void Start()
     {
+        pausePanel.SetActive(true);
         menuPanel.SetActive(false); //hides end level screen
     }
 
@@ -27,12 +32,14 @@ public class EndLevel : MonoBehaviour
             player.SetActive(false);
             LevelBeat();
         }
+        
     }
 
     public void OnTriggerEnter2D(Collider2D node)
     {
         if (node.gameObject.CompareTag("EndPortal")) //if you reach the end, reset countdown, make player disappear, and beat level
         {
+            
             Time.timeScale = 0f;
             player.SetActive(false);
             LevelBeat();
@@ -53,8 +60,9 @@ public class EndLevel : MonoBehaviour
         }
         
         //shows end level screen
-        
+        pausePanel.SetActive(false); //hides pause button
         menuPanel.SetActive(true);
+        compAnim.SetBool("playIt", true); //plays menu panel animation
         scoringManager.GetComponent<Scoring>().AssignStars();
         
         //if a new level was beaten, unlock the next one
