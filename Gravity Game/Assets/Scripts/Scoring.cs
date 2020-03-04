3b0388cd038be1289a4d3a9be77c7f0e38b90a6c
Connor Heath <cmheath2303@yahoo.com>
2020-03-04 10:21:41 -0800

    changes to scoring

--- Gravity Game/Assets/Scripts/Scoring.cs
+++ Gravity Game/Assets/Scripts/Scoring.cs
@@ -20,11 +20,11 @@ public class Scoring : MonoBehaviour
         times[2] = new [] {2.5f,7.5f,12f};
         times[3] = new [] {4f,8f,16f};
         times[4] = new [] {5f,10,17};
-        times[5] = new [] {2f,7.5f,12f};
+        times[5] = new [] {3f,7.5f,12f};
         times[6] = new [] {4f,8f,16f};
         times[7] = new [] {2f,7.5f,12f};
         times[8] = new [] {20f,35f,60f};
-        times[9] = new[] {4f, 7f, 15f};
+        times[9] = new [] {4f, 7f, 15f};
         times[10] = new[] {4f, 7f, 15f};
         times[11] = new[] {4f, 7f, 15f};
         times[12] = new[] {15f, 45f, 90f};
@@ -36,22 +36,27 @@ public class Scoring : MonoBehaviour
         if (PlayerData.time < times[PlayerData.selectedLevel][2])
         {
             compAnim.SetTrigger("star11");
+            //Debug.Log("THIS IS STAR 1 " + starsPerLevel[PlayerData.selectedLevel]);
+            //Debug.Log("This is the time: " + PlayerData.time);
             if (starsPerLevel[PlayerData.selectedLevel] < 1)
             {
                 starsPerLevel[PlayerData.selectedLevel] = 1;
+                //Debug.Log("THIS IS STAR 1a " + starsPerLevel[PlayerData.selectedLevel]);
             }
         }
         if (PlayerData.time < times[PlayerData.selectedLevel][1])
-            compAnim.SetTrigger("star22");
         {
+            compAnim.SetTrigger("star22");
+            //Debug.Log("THIS IS STAR 2 " + starsPerLevel[PlayerData.selectedLevel]);
             if (starsPerLevel[PlayerData.selectedLevel] < 2)
             {
                 starsPerLevel[PlayerData.selectedLevel] = 2;
+                //Debug.Log("THIS IS STAR 2a " + starsPerLevel[PlayerData.selectedLevel]);
             }
         }
         if (PlayerData.time < times[PlayerData.selectedLevel][0])
-            compAnim.SetTrigger("star33");
         {
+            compAnim.SetTrigger("star33");
             if (starsPerLevel[PlayerData.selectedLevel] < 3)
             {
                 starsPerLevel[PlayerData.selectedLevel] = 3;
