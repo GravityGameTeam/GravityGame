9fa24793d05ecad4e882dfe2746c24a72d5e300a
Connor Heath <cmheath2303@yahoo.com>
2020-03-13 12:11:17 -0700

    push

---
+++ Gravity Game/Assets/Scripts/DoubleScroll.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class DoubleScroll : MonoBehaviour
+{
+    public GameObject levels;
+    public GameObject stars;
+    public Vector3 rectPosition;
+    public GameObject starsImagePrefab;
+    public GameObject starsImageContainer;
+
+    public void Update()
+    {
+        //Change the current horizontal scroll position.
+        rectPosition = levels.transform.position;
+        rectPosition.z = 100;
+        rectPosition.y -= 3;
+        stars.transform.position = rectPosition;
+    }
+
+    public void Start()
+    {
+        Sprite[] thumbnails = Resources.LoadAll<Sprite>("StarPictures");
+        for (int i = 1; i <= PlayerData.farthestLevel; i++)
+        {
+            if (Scoring.starsPerLevel[i] == 1)
+            {
+                GameObject container = Instantiate(starsImagePrefab) as GameObject;
+                //container.GetComponent<Image>().sprite = ;
+                container.transform.SetParent(starsImageContainer.transform, false);
+
+            }
+        }
+    }
+}
